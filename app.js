e32c2bb68522b099af16d1eb1e4a72cb353867be
arifire21 <ariwolfpup@icloud.com>
2022-03-30 13:19:13 -0400

    initial

---
+++ app.js
@@ -0,0 +1,30 @@
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+const cameraView = document.querySelector("#camera-canvas"),
+    cameraOutput = document.querySelector("#pic-output"),
+    cameraSensor = document.querySelector("#camera-view"),
+    cameraTrigger = document.querySelector("#camera-btn")
+    
+// Access the device camera and stream to canvas
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+// Take a picture when button is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+
+    //todo, change to button listener
+};// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
