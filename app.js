6a9d2b6734f38d17acd74f1b2935463bcda4122f
arifire21 <ariwolfpup@icloud.com>
2022-04-06 16:02:24 -0400

    camera now takes up whole screen

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // face back camera by default
 var constraints = { video: { facingMode: "environment" }, audio: false };
 
+const cameraContainer = document.getElementById("full-cam-container");
 const cameraView = document.querySelector("#camera-view");
 const cameraOutput = document.querySelector("#pic-output");
-const cameraSensor = document.querySelector("#camera-canvas");
+const cameraCanvas = document.querySelector("#camera-canvas");
 const cameraButton = document.querySelector("#camera-btn");
 
 const startButton = document.querySelector("#activate-btn");
@@ -12,7 +13,27 @@ const saveButton = document.querySelector("#upload-btn");
     
 // Access the device camera and stream to canvas
 function cameraStart() {
-    document.getElementById("full-cam-container").style.display = "block";
+    //get screen size on start
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+
+    //adjust sizes of everything #camera, #camera-view, #camera-canvas
+    cameraContainer.style.width = windowWidth + "px";
+    cameraContainer.style.height = windowHeight + "px";
+    
+    cameraView.style.width = windowWidth + "px";
+    cameraView.style.height = windowHeight + "px";
+
+    cameraCanvas.style.width = windowWidth + "px";
+    cameraCanvas.style.height = windowHeight + "px";
+
+    //make output slightly smaller?
+    //todo set width when actually saving img
+    cameraOutput.style.width = (windowWidth-100) + "px";
+    cameraOutput.style.height = (windowHeight-100) + "px";
+
+    //show camera, hide start button
+    cameraContainer.style.display = "block";
     startButton.style.display = "none";
     
     if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
@@ -37,19 +58,19 @@ function getFileName(str) {
 
 // Take a picture when button is tapped
 cameraButton.addEventListener("click", function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/png");
+    cameraCanvas.width = cameraView.videoWidth;
+    cameraCanvas.height = cameraView.videoHeight;
+    cameraCanvas.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraCanvas.toDataURL("image/png");
     cameraOutput.classList.add("taken");
 
     //hide anything camera related to show the preview / option buttons
-    document.getElementById("full-cam-container").style.display = "none";
+    cameraContainer.style.display = "none";
     document.getElementById("btn-container").style.display = "block";
 });
 
 retakeButton.addEventListener("click", function() {
-    document.getElementById("full-cam-container").style.display = "block";
+    cameraContainer.style.display = "block";
     cameraOutput.src = "//:0";
     cameraOutput.classList.remove("taken");
     document.getElementById("btn-container").style.display = "none";
