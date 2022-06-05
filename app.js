dcb277993e2b83264a1702ec5709aac7d53c70a0
arifire21 <ariwolfpup@icloud.com>
2022-06-05 11:59:00 -0400

    smaller res so it doesnt go off screen?

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ function startCamera() {
         audio: false,
         video: {
             facingMode: "environment",
-            // width: { min: windowWidth, ideal: windowWidth },
-            // height: { min: calc, ideal: calc },
+            width: 320,
+            height: 320
         }
     };
 
