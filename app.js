1988066515fd22a9bb49721eb142d9cb4313439a
arifire21 <ariwolfpup@icloud.com>
2022-06-07 01:18:11 -0400

    camera sized wit proper width and height

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ const cameraButton = document.querySelector("#camera-btn");
 const startButton = document.querySelector("#activate-btn");
 const retakeButton = document.querySelector("#retake-btn");
 const doneButton = document.querySelector("#done-btn");
+doneButton.disabled = true;
+doneButton.style.opacity="0.5";
 
 //VARIABLES
 const aspectRatioConst = 1.1;
+var windowWidth = 0;
+var calc=0;
 
 //------------------------------------------------------//
 
@@ -22,10 +26,10 @@ function startCamera() {
     document.getElementById("activate-btn-container").style.display = "none";
 
     //get screen size on start
-    const windowWidth = window.innerWidth;
+    windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
 
-    const calc = (windowWidth * aspectRatioConst);
+    calc = (windowWidth * aspectRatioConst);
 
     // format vid constraints face back camera by default
     const constraints = {
@@ -39,8 +43,8 @@ function startCamera() {
 
     // document.querySelector("#actual-cam").style.height = calc + "px";
 
-    cameraVid.style.width = windowWidth + "px";
-    cameraVid.style.height = calc + "px";
+    cameraVid.width = windowWidth;
+    cameraVid.height = calc;
 
     document.querySelector("#wireframe").style.width = windowWidth + "px";
     document.querySelector("#wireframe").style.height = calc + "px";
@@ -50,6 +54,8 @@ function startCamera() {
 
     cameraOutput.width = windowWidth;
     cameraOutput.height = calc;
+    // cameraOutput.style.width = windowWidth + "px";
+    // cameraOutput.style.height = calc + "px";
 
     cameraCanvas.width = windowWidth;
     cameraCanvas.height = calc;
@@ -97,7 +103,7 @@ function getFileName() {
 //EVENT LISTENERS
 // Take a picture when button is tapped
 cameraButton.addEventListener("click", function() {
-    cameraCanvas.getContext("2d").drawImage(cameraVid, 0, 0);
+    cameraCanvas.getContext("2d").drawImage(cameraVid, 0, 0, windowWidth, calc, 0, 0, windowWidth, calc);
     cameraOutput.src = cameraCanvas.toDataURL("image/png");
     cameraOutput.classList.add("taken");
 
@@ -109,7 +115,7 @@ cameraButton.addEventListener("click", function() {
 retakeButton.addEventListener("click", function() {
     cameraOutput.src = "//:0";
     cameraOutput.classList.remove("taken");
-    cameraCanvas.getContext("2d").clearRect(0, 0, cameraCanvas.width, cameraCanvas.height);
+    // cameraCanvas.getContext("2d").clearRect(0, 0, cameraCanvas.width, cameraCanvas.height);
     cameraContainer.style.display = "block";
     document.getElementById("result-container").style.display = "none";
 });
